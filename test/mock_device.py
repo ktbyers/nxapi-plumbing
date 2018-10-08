e47971e9dfc88b35be14baf233516709238eeb94
Kirk Byers <ktbyers@twb-tech.com>
2018-10-07 17:45:33 -0700

    Cleaning up some of the mock code

--- test/mock_device.py
+++ test/mock_device.py
@@ -14,38 +14,24 @@ class FakeResponse(object):
         self.text = ""
 
 
-def mock_post(url, timeout, data, headers, auth, verify):
+def mock_post(url, timeout, data, headers, auth, verify, api_type="jsonrpc"):
     """Look up the response based on the URL and payload."""
 
     # Construct the path to search for the mocked data
     # e.g. ./mocked_data/jsonrpc_show_hostname/response.json
     base_dir = "test/mocked_data"
-    data = json.loads(data)
-    if data[0].get("jsonrpc"):
-        api_type = "jsonrpc"
-    api_cmd = data[0]["params"]["cmd"]
+    if api_type == "jsonrpc":
+        data = json.loads(data)
+        api_cmd = data[0]["params"]["cmd"]
+        file_ext = "json"
+    elif api_type == "xml":
+        xml_root = etree.fromstring(data)
+        input_obj = xml_root.find("./input")
+        api_cmd = input_obj.text
+        file_ext = "xml"
     api_cmd = api_cmd.replace(" ", "_")
-    file_path = "{base_dir}/{api_type}_{api_cmd}/response.json".format(
-        base_dir=base_dir, api_type=api_type, api_cmd=api_cmd
-    )
-
-    with open(file_path) as f:
-        return f.read()
-
-
-def mock_post_xml(url, timeout, data, headers, auth, verify):
-    """Look up the response based on the URL and payload."""
-
-    # Construct the path to search for the mocked data
-    # e.g. ./mocked_data/jsonrpc_show_hostname/response.json
-    base_dir = "test/mocked_data"
-    xml_root = etree.fromstring(data)
-    input_obj = xml_root.find("./input")
-    api_type = "xml"
-    api_cmd = input_obj.text
-    api_cmd = api_cmd.replace(" ", "_")
-    file_path = "{base_dir}/{api_type}_{api_cmd}/response.xml".format(
-        base_dir=base_dir, api_type=api_type, api_cmd=api_cmd
+    file_path = "{base_dir}/{api_type}_{api_cmd}/response.{file_ext}".format(
+        base_dir=base_dir, api_type=api_type, api_cmd=api_cmd, file_ext=file_ext
     )
 
     with open(file_path) as f:
@@ -107,6 +93,7 @@ class MockRPCClient(RPCClient):
             headers=self.headers,
             auth=HTTPBasicAuth(self.username, self.password),
             verify=self.verify,
+            api_type="jsonrpc",
         )
 
         response_obj = FakeResponse()
@@ -120,13 +107,14 @@ class MockXMLClient(XMLClient):
     def _send_request(self, commands, method="cli_show"):
         payload = self._build_payload(commands, method)
 
-        mock_response = mock_post_xml(
+        mock_response = mock_post(
             self.url,
             timeout=self.timeout,
             data=payload,
             headers=self.headers,
             auth=HTTPBasicAuth(self.username, self.password),
             verify=self.verify,
+            api_type="xml",
         )
 
         response_obj = FakeResponse()
